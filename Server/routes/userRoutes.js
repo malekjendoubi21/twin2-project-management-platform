2846cf626f7d11ee81843223c6861577b032a31a
moetaz <moetaz.benkhedher@esprit.tn>
2025-02-22 23:52:32 +0200

    changePassword method

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, addUser, updateUser, getUser} = require('../controllers/UserController');
+const { getAllUsers, addUser, updateUser, getUser, changePassword} = require('../controllers/UserController');
 const { protection } =require('../controllers/AuthController');
 
 
@@ -8,6 +8,7 @@ router.get('/', protection, getAllUsers);
 router.post('/addUser', protection, addUser);
 router.put('/updateUser/:id', protection, updateUser);
 router.get('/getUser/:id', protection, getUser);
+router.put('/changePassword/:id', protection, changePassword);
 
 
 module.exports = router;
