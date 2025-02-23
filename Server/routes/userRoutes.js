92d7a6a70da2ba235714c432b3cff38fc43330c4
moetaz <moetaz.benkhedher@esprit.tn>
2025-02-24 00:32:09 +0200

    delete methods

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, addUser, updateUser, getUserById, changePassword, getLoggedUser, updateLoggedUserPassword, UpdateLoggeduserData} = require('../controllers/UserController');
+const { getAllUsers, addUser, updateUser, dropUser, getUserById, changePassword, getLoggedUser, updateLoggedUserPassword, UpdateLoggeduserData, deleteLoggedUser} = require('../controllers/UserController');
 const { protection, allowTo } =require('../controllers/AuthController');
 
 
@@ -8,10 +8,12 @@ router.get('/', protection, allowTo('admin'), getAllUsers);
 router.post('/addUser', protection, allowTo('admin'), addUser);
 router.put('/updateUser/:id', protection, allowTo('admin'), updateUser);
 router.get('/getUser/:id', protection, allowTo('admin'), getUserById);
+router.delete('/dropUser/:id', protection, allowTo('admin'), dropUser);
 router.put('/changePassword/:id', protection, changePassword);
 
 router.get('/getMe', protection, getLoggedUser, getUserById);
 router.put('/updateMyPassword', protection, updateLoggedUserPassword);
 router.put('/updateMe', protection, UpdateLoggeduserData);
+router.put('/deleteMe', protection, deleteLoggedUser);
 
 module.exports = router
