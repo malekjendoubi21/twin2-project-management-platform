b9fc0d11be6be717054a76c9bce93be220140345
moetaz <moetaz.benkhedher@esprit.tn>
2025-04-05 03:36:26 +0200

    fixin notification badge

--- Client/src/utils/NotificationSystem.jsx
+++ Client/src/utils/NotificationSystem.jsx
@@ -134,14 +134,17 @@ const NotificationSystem = () => {
         className="btn btn-ghost btn-circle" 
         onClick={toggleNotifications}
       >
-        <div className="indicator">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-          </svg>
-          {unreadCount > 0 && (
-            <span className="badge badge-error badge-sm absolute top-1 right-1">{unreadCount}</span>
-          )}
-        </div>
+<div className="indicator relative">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+  </svg>
+  {unreadCount > 0 && (
+    <span className="absolute -top-1 -left-1 text-[10px] min-w-[16px] h-[16px] px-1 rounded-full bg-error text-white flex items-center justify-center shadow">
+      {unreadCount}
+    </span>
+  )}
+</div>
+
       </button>
 
       {/* Notification Sidebar */}
