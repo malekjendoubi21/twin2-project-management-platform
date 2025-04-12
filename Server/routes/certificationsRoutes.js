375871702a6f2b4e01ab13d13bf829931a303fe1
eyansibi <eya.nsibi@esprit.tn>
2025-04-12 17:00:24 +0100

    add quizz with AI

--- Server/routes/certificationsRoutes.js
+++ Server/routes/certificationsRoutes.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
-
 const certificationController = require('../controllers/CertifiacationsController');
+const authController = require('../controllers/AuthController');
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 
-router.post('/addCertification', certificationController.createCertification);
-router.put('/updateCertification/:id', certificationController.updateCertification);
-router.get('/', certificationController.getAllCertifications);
-router.get('/:id', certificationController.getCertificationById);
-router.delete('/:id', certificationController.deleteCertification);
+// Configurer Cloudinary
+cloudinary.config({
+  cloud_name: 'dwbccq2j6',
+  api_key: '511441296244864',
+  api_secret: 'mDc4YVFPtkiyB2eMa8_FlXRX8QU',
+});
+
+// Configurer MulterStorage avec Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'certifications',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get('/', authController.protection, certificationController.getUserCertifications);
+router.get('/:id', authController.protection, certificationController.getCertificationById);
+router.post('/add', authController.protection, upload.single('image'), certificationController.createCertification);
+router.put('/update/:id', authController.protection, upload.single('image'), certificationController.updateCertification);
+router.delete('/:id', authController.protection, certificationController.deleteCertification);
 
 module.exports = router;
