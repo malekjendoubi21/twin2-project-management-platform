cd3a1a1b4e04241d819217222630db6297b3796e
moetaz <moetaz.benkhedher@esprit.tn>
2025-04-16 00:54:55 +0200

    jenkins

--- Server/.eslintrc.js
+++ Server/.eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module"
-  },
-  rules: {
-    "no-unused-vars": "warn",
-    "no-undef": "error"
-  },
-  globals: {
-    process: true,
-    __dirname: true
-  }
-};
+    env: {
+      node: true,
+      es2021: true
+    },
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  };
